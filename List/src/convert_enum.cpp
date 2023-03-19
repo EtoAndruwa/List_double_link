2dabc199634b77a470e21b531f94d8d1652de53a
Andrey Timonin <timonina1909@gmail.com>
2023-03-19 15:30:26 +0300

    Here is the first commit using Atom ide

--- List/src/convert_enum.cpp
+++ List/src/convert_enum.cpp
@@ -1,25 +1,25 @@
-// #include "List_double_link.h"
+#include "List_double_link.h"
 
-// const char* enum_to_string(size_t error_code)
-// {
-//     #define CONVERT_TO_STRING(enum_code) \
-//         case enum_code:                     \    
-//             return #enum_code;                \
+const char* enum_to_string(size_t error_code)
+{
+    #define CONVERT_TO_STRING(enum_code) \
+        case enum_code:                     \
+            return #enum_code;                \
 
-//     switch(error_code)
-//     {
+    switch(error_code)
+    {
 
-//     CONVERT_TO_STRING(LIST_OK)
-//     CONVERT_TO_STRING(ERR_OPEN_DUMP)
-//     CONVERT_TO_STRING(ERR_CLOSE_DUMP)
-//     CONVERT_TO_STRING(ERR_CALLOC_NODE_ARR)
-//     CONVERT_TO_STRING(ERR_REALLOC_NODE_ARR)
-//     CONVERT_TO_STRING(ERR_LIST_FULL)
-//     CONVERT_TO_STRING(ERR_LIST_EMPTY)
+    CONVERT_TO_STRING(LIST_OK)
+    CONVERT_TO_STRING(ERR_OPEN_DUMP)
+    CONVERT_TO_STRING(ERR_CLOSE_DUMP)
+    CONVERT_TO_STRING(ERR_CALLOC_NODE_ARR)
+    CONVERT_TO_STRING(ERR_REALLOC_NODE_ARR)
+    CONVERT_TO_STRING(ERR_LIST_FULL)
+    CONVERT_TO_STRING(ERR_LIST_EMPTY)
 
-//     #undef CONVERT_TO_STRING
+    #undef CONVERT_TO_STRING
 
-//     default:
-//         return "NEW_ERROR_CODE";
-//     }
-// }
+    default:
+        return "NEW_ERROR_CODE";
+    }
+}
