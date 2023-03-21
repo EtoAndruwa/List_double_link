b569903ffc641526d5aa141d3708fd65342c816e
Andrey Timonin <timonina1909@gmail.com>
2023-03-21 03:44:50 +0300

    up-to-date file

--- List/src/main.cpp
+++ List/src/main.cpp
@@ -5,35 +5,43 @@ int main()
     list* list_str = nullptr;
 
     list_str = list_ctor(10);
-    list_str->nodes_arr[0].value = 0;
-    list_str->nodes_arr[1].value = 1;
-    list_str->nodes_arr[2].value = 2;
-    list_str->nodes_arr[3].value = 3;
-    list_str->nodes_arr[4].value = 4;
-    list_str->nodes_arr[5].value = 5;
-    list_str->nodes_arr[6].value = 6;
+    // list_str->nodes_arr[0].value = 0;
+    // list_str->nodes_arr[1].value = 1;
+    // list_str->nodes_arr[2].value = 2;
+    // list_str->nodes_arr[3].value = 3;
+    // list_str->nodes_arr[4].value = 4;
+    // list_str->nodes_arr[5].value = 5;
+    // list_str->nodes_arr[6].value = 6;
 
-    list_str->head_node = 0;
-    list_str->tail_node = 6;
-    list_str->free_node = 7;
+    // list_str->head_node = 0;
+    // list_str->tail_node = 6;
+    // list_str->free_node = 7;
 
-    list_str->nodes_arr[0].next = 3;
-    list_str->nodes_arr[1].next = 6;
-    list_str->nodes_arr[2].next = 4;
-    list_str->nodes_arr[3].next = 2;
-    list_str->nodes_arr[4].next = 5;
-    list_str->nodes_arr[5].next = 1;
-    list_str->nodes_arr[6].next = -1;
+    // list_str->nodes_arr[0].next = 3;
+    // list_str->nodes_arr[1].next = 6;
+    // list_str->nodes_arr[2].next = 4;
+    // list_str->nodes_arr[3].next = 2;
+    // list_str->nodes_arr[4].next = 5;
+    // list_str->nodes_arr[5].next = 1;
+    // list_str->nodes_arr[6].next = -1;
 
-    list_str->nodes_arr[0].prev = -1;
-    list_str->nodes_arr[1].prev = 5;
-    list_str->nodes_arr[2].prev = 3;
-    list_str->nodes_arr[3].prev = 0;
-    list_str->nodes_arr[4].prev = 2;
-    list_str->nodes_arr[5].prev = 4;
-    list_str->nodes_arr[6].prev = 1;
+    // list_str->nodes_arr[0].prev = -1;
+    // list_str->nodes_arr[1].prev = 5;
+    // list_str->nodes_arr[2].prev = 3;
+    // list_str->nodes_arr[3].prev = 0;
+    // list_str->nodes_arr[4].prev = 2;
+    // list_str->nodes_arr[5].prev = 4;
+    // list_str->nodes_arr[6].prev = 1;
 
-    list_str->cur_num_of_nodes = 7;
+    // list_str->cur_num_of_nodes = 7;
+
+    push_after(list_str, 0 ,1);
+    push_after(list_str, 0 ,2);
+    push_after(list_str, 1 ,3);
+    delete_node(list_str, 0);
+    push_after(list_str, 2 ,4);
+    push_after(list_str, 3 ,5);
+    push_after(list_str, 1 ,6);
 
     create_graph_jpg(list_str, "");
     // push_after(list_str, 0, 100);
